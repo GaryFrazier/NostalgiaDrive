6d7a93131d900439e489555bc96e2bb5d871f709
Gary Frazier <garyfrazier95@yahoo.com>
2022-08-14 23:34:16 -0400

    start menu stuff

---
+++ pages/components/startMenu/index.js
@@ -0,0 +1,11 @@
+import styles from './startMenu.module.css'
+import Image from 'next/image'
+import DiskDrive from '../../../public/diskDrive.png'
+
+export default function StartMenu() {
+    return (
+        <div className={styles.startMenu}>
+            hi
+        </div>
+    )
+}
--- pages/components/taskbar/startButton.js
+++ pages/components/taskbar/startButton.js
@@ -1,17 +1,24 @@
 import styles from './taskbar.module.css'
+import React, { useState } from 'react';
 import Image from 'next/image'
 import DiskDrive from '../../../public/diskDrive.png'
+import StartMenu from '../startMenu'
 
 export default function StartButton() {
+    const [startMenuOpen, setStartMenuOpen] = useState(false);
+
     return (
-        <div className={styles.startButton}>
-            <Image
-                src={DiskDrive}
-                alt="Nostalgia Drive Logo"
-                width={20}
-                height={20}
-            />
-            <span>Nostalgia Drive</span>
+        <div>
+            { startMenuOpen && <StartMenu /> }
+            <button className={styles.startButton} onClick={() => setStartMenuOpen(!startMenuOpen)}>
+                <Image
+                    src={DiskDrive}
+                    alt="Nostalgia Drive Logo"
+                    width={20}
+                    height={20}
+                />
+                <span>Nostalgia Drive</span>
+            </button>
         </div>
     )
 }
