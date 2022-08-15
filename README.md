3cbfebd639c80b468330e3946f5d526fe18ee000
Gary Frazier <garyfrazier95@yahoo.com>
2022-08-14 20:29:21 -0400

    remove

--- README.md
+++
@@ -1,4 +0,0 @@
-# TheVirtualPlaza
-Ｗｅｌｃｏｍｅ  ｔｏ ｔｈｅ  仮想プラザ  Ｖｉｒｔｕａｌ  Ｐｌａｚａ
-
-desktop code forked from https://github.com/chairuosen/desktop-simulation
--- src/main.js
+++
@@ -1,8 +0,0 @@
-var Vue = require('vue');
-var jQuery = require('jquery');
-window.$ = jQuery;
-window.jQuery = jQuery;
-window.Vue = Vue;
-window.$event = require('service/event');
-
-new Vue(require('views/root.vue'));
