f1a09b8fffd6804bccda863d83efe2c8676c3013
Gary Frazier <garyfrazier95@yahoo.com>
2022-08-17 17:02:51 -0400

    playlist swapping

--- pages/api/videos.js
+++ pages/api/videos.js
@@ -1,10 +1,10 @@
 
 export function getVideos() {
     let testData = {
-        popular: ["dTn8pKWTohw"],
+        popular: ["dTn8pKWTohw", "Vfe5g0twoXk"],
         random: ["Vfe5g0twoXk"],
         favorites: ["H5D3d4_O0e4"],
     }
-    return Promise.resolve({})
+    return Promise.resolve(testData)
         .then(data => data)
 }
--- pages/components/apps/video/index.js
+++ pages/components/apps/video/index.js
@@ -6,13 +6,13 @@ import { faPlay, faPause, faForwardStep, faBackwardStep, faHeart, faThumbsUp, fa
 
 import { getVideos } from '../../..//api/videos'
 
-let videoElement = null;
-
 export default function Video() {
     const [isPaused, setIsPaused] = useState(false)
     const [playlist, setPlaylist] = useState("popular")
     const [videos, setVideos] = useState({})
     const [videoIndex, setVideoIndex] = useState(0)
+    const [videoElement, setVideoElement] = useState(null)
+    const [loading, setLoading] = useState(true)
 
     const togglePause = () => {
         setIsPaused(!isPaused);
@@ -20,8 +20,9 @@ export default function Video() {
 
     useEffect(() => { 
         async function fetchData() {
-            return await getVideos()
-            setVideos(videos)
+            let vids = await getVideos()
+            setVideos(vids)
+            setLoading(false)
           }
           fetchData();
     }, [])
@@ -45,17 +46,21 @@ export default function Video() {
       useEffect(() => {   
         if (videoElement && videos && videos[playlist]) {
           try {
-            videoElement.target.loadPlaylist(videos[playlist], videoIndex % videos[playlist].length, 0);
+                videoElement.target.loadPlaylist(videos[playlist], videoIndex % videos[playlist].length, 0);
           } catch(e) {
               
           }
           
         }
-      }, [playlist, videos]);
+      }, [playlist, videos, videoElement]);
 
     const _onReady = (event) => {
-        videoElement = event;
-    };
+        setVideoElement(event);
+    }
+
+    const _onPlay = (event) => {
+        setVideoIndex(videoIndex + 1)
+    }
 
     const onChangeValue = (event) => {
         setPlaylist(event.target.value)
@@ -63,28 +68,31 @@ export default function Video() {
 
     return (
         <div className={styles.video}>
-            <YouTube
-                videoId="d3SV2tKr6BY"                // defaults -> ''
-                id="ytPlayerVideo"                       // defaults -> ''
-                className={styles.player}                // defaults -> ''
-                opts={{
-                    playerVars: {
-                        autoplay: 1
-                      }
-                }}
-                onReady={_onReady}
-                /*iframeClassName={string}          // defaults -> ''
-                style={object}                    // defaults -> {}
-                title={string}                    // defaults -> ''
-                loading={string}                  // defaults -> undefined
-                onPlay={func}                     // defaults -> noop
-                onPause={func}                    // defaults -> noop
-                onEnd={func}                      // defaults -> noop
-                onError={func}                    // defaults -> noop
-                onStateChange={func}              // defaults -> noop
-                onPlaybackRateChange={func}       // defaults -> noop
-                onPlaybackQualityChange={func}    // defaults -> noop*/
-            />
+            {loading ? <div className={styles.loading}><span>Loading...</span></div>
+                : <YouTube
+                    videoId={videos && videos["popular"] ? videos["popular"][0] : null}
+                    id="ytPlayerVideo"                       // defaults -> ''
+                    className={styles.player}                // defaults -> ''
+                    opts={{
+                        playerVars: {
+                            autoplay: 1
+                        }
+                    }}
+                    onReady={_onReady}
+                    onPlay={_onPlay}
+                    /*iframeClassName={string}          // defaults -> ''
+                    style={object}                    // defaults -> {}
+                    title={string}                    // defaults -> ''
+                    loading={string}                  // defaults -> undefined
+                    onPause={func}                    // defaults -> noop
+                    onEnd={func}                      // defaults -> noop
+                    onError={func}                    // defaults -> noop
+                    onStateChange={func}              // defaults -> noop
+                    onPlaybackRateChange={func}       // defaults -> noop
+                    onPlaybackQualityChange={func}    // defaults -> noop*/
+                />
+                }
+                
             <div className={styles.bottomBar}>
                 <button onClick={togglePause} className={styles.playerButton}>
                     <FontAwesomeIcon icon={isPaused ? faPlay : faPause} />
